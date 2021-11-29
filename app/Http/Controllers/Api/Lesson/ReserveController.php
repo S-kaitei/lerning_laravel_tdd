5861f6ce3b7da9f204905693736053e6e462a5ae
S-kaitei <oren.hagetaka@gmail.com>
2021-11-29 18:03:59 +0900

    chapter3-5:green_異常系

--- app/Http/Controllers/Api/Lesson/ReserveController.php
+++ app/Http/Controllers/Api/Lesson/ReserveController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\Lesson;
 use App\Http\Controllers\Controller;
 use App\Models\Lesson;
 use App\Models\Reservation;
+use Exception;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
 
@@ -13,6 +14,11 @@ class ReserveController extends Controller
     public function __invoke(Lesson $lesson)
     {
         $user = Auth::user();
+        try {
+            $user->canReserve($lesson);
+        } catch (Exception $e) {
+            return response()->json(['error' => '予約できません。：' . $e->getMessage()], Response::HTTP_CONFLICT);
+        }
         $reservation = Reservation::create(['lesson_id' => $lesson->id, 'user_id' => $user->id]);
 
         return response()->json($reservation, Response::HTTP_CREATED);
